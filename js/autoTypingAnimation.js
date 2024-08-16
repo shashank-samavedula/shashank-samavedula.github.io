d3d2dec9717afc7d9ed1793b096b35263f3453e0
S Shashank <84611995+shashank-samavedula@users.noreply.github.com>
2024-08-16 23:24:35 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hey there! I'm S Shashank, a Computer Science Post-Grad with a knack for turning complex tech into something magical. I’ve rolled up my sleeves as a Data Engineer Intern, crafting slick data pipelines with Python and MongoDB. When I'm not doing that, you'll find me demystifying Algorithms and Computer Networking as a Graduate Teaching Assistant. I speak fluent Python, Java, and SQL, and love tinkering with TensorFlow, PyTorch, NumPy, and Pandas. My brain thrives on Machine Learning, Complexity Theory, and Quantum Computing. Oh, and I’m all about using tech to make the world a better place!."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
